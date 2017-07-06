9290457b00c0023ecfb976bb8194d40496848634
KarthigaAngellena <karthikakeerthi25@gmail.com>
2017-07-06 17:53:59 +0530

    Create Copy.java

---
+++ Copy.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+public class Copy
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int a;
+a=sc.nextInt();
+if(a>0)
+{
+System.out.println("Positive");
+}
+else if(a<0)
+{
+System.out.println("Negative");
+}
+else
+{
+System.out.prinln("Zero");
+}
+}
+}
