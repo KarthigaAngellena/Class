fdd3b26bb229eb79f80969b6013b7c4a41014fc6
KarthigaAngellena <karthikakeerthi25@gmail.com>
2017-07-06 18:15:46 +0530

    Update Vowels.java

--- Vowels.java
+++ Vowels.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 public class Vowels
 {
-public static void main(String args[])throws IOException
+public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
 Char c;
