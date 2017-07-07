7ab7b9f9fc67fcb0c0dd3ff168569c3c0a9c52dd
KarthigaAngellena <karthikakeerthi25@gmail.com>
2017-07-07 09:59:54 +0530

    Create Alpha.java

---
+++ Alpha.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+public class Alpha
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+char c;
+c=sc.next().charAt(0);
+if(('c'='a')||('c'='b')||('c'='c')||('c'='d')||('c'='e')||('c'='f')||('c'='g')||('c'='h')||('c'='i')||('c'='j')||('c'='k')||('c'='l')||
+   ('c'='m')||('c'='n')||('c'='o')||('c'='p')||('c'='q')||('c'='r')||('c'='s')||('c'='t')||('c'='u')||('c'='v')||('c'='w')||('c'='x')||
+   ('c'='y')||('c'='z'))
+   {
+   System.out.println("Given character is Alphabet");
+   }
+ else if(('c'='A')||('c'='B')||('c'='C')||('c'='D')||('c'='E')||('c'='F')||('c'='G')||('c'='H')||('c'='I')||('c'='J')||('c'='K')||('c'='L')||
+         ('c'='M')||('c'='N')||('c'='O')||('c'='P')||('c'='Q')||('c'='R')||('c'='S')||('c'='T')||('c'='U')||('c'='V')||('c'='W')||('c'='X')||
+         ('c'='Y')||('c'='Z'))
+        {
+        System.out.println("Given character is Alphabet");
+        }
+  else
+  {
+  System.out.println("Given character is not Alphabet);
+  }
+  }
+  }
